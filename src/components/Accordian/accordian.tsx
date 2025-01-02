0829e7af1653eb456293e787ed7bd9d4f67bc5d6
Anurag <anurag3487@gmail.com>
2025-01-03 00:40:54 +0530

    accordian component click event

--- src/components/Accordian/accordian.tsx
+++ src/components/Accordian/accordian.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import accData from "./data";
 
 function Accordian() {
+  const [selected, setSelected] = useState(0);
+
+  function handleSingleClick(currentId: number) {
+    console.log(currentId);
+    setSelected(currentId);
+  }
   return (
     <>
       <h2>Accordian</h2>
@@ -8,9 +15,16 @@ function Accordian() {
         {accData && accData.length ? (
           <div>
             {accData.map((item) => (
-              <div>
-                <p className="question-item">{item.question}</p>
-                <span>+</span>
+              <div className="container">
+                <div
+                  key={item.id}
+                  className="question-item"
+                  onClick={() => handleSingleClick(item.id)}
+                >
+                  <p>{item.question}</p>
+                  <span>+</span>
+                </div>
+                {selected == item.id ? <div>{item.answer}</div> : null}
               </div>
             ))}
           </div>
