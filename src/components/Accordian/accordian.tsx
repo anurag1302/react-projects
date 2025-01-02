ad06cd4d2dc1ada7dbc6d0ff257b0035992f4c29
Anurag <anurag3487@gmail.com>
2025-01-03 00:16:01 +0530

    accordian - data pull

--- src/components/Accordian/accordian.tsx
+++ src/components/Accordian/accordian.tsx
@@ -1,7 +1,23 @@
+import accData from "./data";
+
 function Accordian() {
   return (
     <>
       <h2>Accordian</h2>
+      <div>
+        {accData && accData.length ? (
+          <div>
+            {accData.map((item) => (
+              <div>
+                <p className="question-item">{item.question}</p>
+                <span>+</span>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>No data found</div>
+        )}
+      </div>
     </>
   );
 }
