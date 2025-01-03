9d7159cd190a2bf11baf9383ea3b2470ed720d36
Anurag <anurag3487@gmail.com>
2025-01-04 00:09:52 +0530

    random RGB color button addition

--- src/components/Color/colorPicker.tsx
+++ src/components/Color/colorPicker.tsx
@@ -3,23 +3,43 @@ import { useState } from "react";
 function ColorPicker() {
   const [color, setColor] = useState("red");
 
-  function getRandom() {
+  function getRandomHexNumber() {
     let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
     let randomCharacter = hexArray[Math.floor(Math.random() * hexArray.length)];
     return randomCharacter;
   }
 
+  function getRandomRGBNumber() {
+    //to get a random number in a range -> 0 to 256 (256 not included)
+    //Math.random()*(max-min)+min;
+    return Math.floor(Math.random() * (256 - 0) + 0);
+  }
+
   function getHEXColor() {
     let str = "";
     for (let i = 0; i < 6; i++) {
-      str = str + getRandom();
+      str = str + getRandomHexNumber();
     }
     let randomColor = "#" + str;
     setColor(randomColor);
   }
 
+  function getRGBColor() {
+    let outer = "";
+    for (let i = 0; i < 3; i++) {
+      outer = outer + "," + getRandomRGBNumber();
+    }
+    outer = outer.replace(",", "");
+    let randomColor = `RGB(${outer})`;
+    console.log(randomColor);
+    setColor(randomColor);
+  }
+
   function handleButtonClick() {
     getHEXColor();
+  }
+  function handleButtonClickRGB() {
+    getRGBColor();
   }
   return (
     <>
@@ -35,7 +55,21 @@ function ColorPicker() {
           borderRadius: "5px",
         }}
       >
-        Generate Random Color
+        Generate Random Color HEX
+      </button>
+      <button
+        onClick={handleButtonClickRGB}
+        style={{
+          marginBottom: "10px",
+          fontWeight: "bold",
+          backgroundColor: "black",
+          color: "white",
+          cursor: "pointer",
+          borderRadius: "5px",
+          marginLeft: "5px",
+        }}
+      >
+        Generate Random Color RGB
       </button>
 
       <div
