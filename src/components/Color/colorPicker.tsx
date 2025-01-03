47559eb367ade1f9d76f5ab78cb1f8e3002df12d
Anurag <anurag3487@gmail.com>
2025-01-03 22:37:32 +0530

    setup color context

--- src/components/Color/colorPicker.tsx
+++ src/components/Color/colorPicker.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 function ColorPicker() {
+  const [color, setColor] = useState("red");
+
+  function handleButtonClick() {
+    setColor("green");
+  }
   return (
     <>
       <h2>Color Picker</h2>
+      <button
+        onClick={handleButtonClick}
+        style={{
+          marginBottom: "10px",
+          fontWeight: "bold",
+          backgroundColor: "black",
+          color: "white",
+          cursor: "pointer",
+          borderRadius: "5px",
+        }}
+      >
+        Generate Random Color
+      </button>
+      <div
+        style={{
+          backgroundColor: color,
+          width: "100vw",
+          height: "100vh",
+        }}
+      ></div>
     </>
   );
 }
