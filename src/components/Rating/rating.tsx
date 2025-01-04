1f182c7c9f7bef5c5e4bf48f896679ec7891da29
anurag1302 <anurag3487@gmail.com>
2025-01-04 23:05:23 +0530

    array map

--- src/components/Rating/rating.tsx
+++ src/components/Rating/rating.tsx
@@ -1,7 +1,11 @@
 function Rating() {
+  let arr = Array.from(Array(10).keys());
   return (
     <>
       <h2>Star Rating</h2>
+      {arr.map((_, index) => {
+        return <span key={index + 1}>:D</span>;
+      })}
     </>
   );
 }
