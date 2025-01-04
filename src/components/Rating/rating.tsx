19901747d15d1279864ff597f474e5af9aff5da0
anurag1302 <anurag3487@gmail.com>
2025-01-04 23:09:21 +0530

    FaStar component in rating

--- src/components/Rating/rating.tsx
+++ src/components/Rating/rating.tsx
@@ -1,10 +1,16 @@
+import { FaStar } from "react-icons/fa";
+
 function Rating() {
   let arr = Array.from(Array(10).keys());
   return (
     <>
       <h2>Star Rating</h2>
       {arr.map((_, index) => {
-        return <span key={index + 1}>:D</span>;
+        return (
+          <span key={index + 1}>
+            <FaStar />
+          </span>
+        );
       })}
     </>
   );
