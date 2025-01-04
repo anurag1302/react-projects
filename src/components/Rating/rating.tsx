44e3f554877d66f2c87fce6e953954141ae792f8
anurag1302 <anurag3487@gmail.com>
2025-01-04 23:37:57 +0530

    hover and setHover

--- src/components/Rating/rating.tsx
+++ src/components/Rating/rating.tsx
@@ -1,13 +1,40 @@
+import { useState } from "react";
 import { FaStar } from "react-icons/fa";
+import "./styles.css";
 
 function Rating() {
   let arr = Array.from(Array(10).keys());
+
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(currentIndex: number) {
+    console.log(currentIndex);
+    setRating(currentIndex);
+  }
+
+  function handleMouseEnter(currentIndex: number) {
+    setHover(currentIndex);
+    console.log(currentIndex);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
   return (
     <>
       <h2>Star Rating</h2>
       {arr.map((_, index) => {
+        index = index + 1;
         return (
-          <span key={index + 1}>
+          <span
+            key={index}
+            onClick={() => handleClick(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={handleMouseLeave}
+            className={index <= (rating || hover) ? "active" : "passive"}
+          >
             <FaStar />
           </span>
         );
