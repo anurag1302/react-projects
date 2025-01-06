ba77d71959f105053cb1b27cd3360f13b979962a
anurag1302 <anurag3487@gmail.com>
2025-01-06 23:46:56 +0530

    image slider images and buttons done

--- src/components/ImageSlider/imageSlider.tsx
+++ src/components/ImageSlider/imageSlider.tsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
+const img1 = "src/assets/images/donald.png";
+const img2 = "src/assets/images/garfield.png";
+const img3 = "src/assets/images/jerry.png";
+const img4 = "src/assets/images/mickey.png";
+const img5 = "src/assets/images/pluto.png";
+const img6 = "src/assets/images/tom.png";
+
+const IMAGES = [img1, img2, img3, img4, img5, img6];
+
 function ImageSilder() {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  function showPreviousImage() {}
+
+  function showNextImage() {}
   return (
     <>
-      <h1>Image Slider</h1>
+      <img src={IMAGES[currentImage]} style={{ width: "30%", height: "30%" }} />
+      <button
+        onClick={showPreviousImage}
+        style={{
+          cursor: "pointer",
+          borderRadius: "2px",
+          fontSize: "20px",
+        }}
+      >
+        Previous
+      </button>
+      <button
+        onClick={showNextImage}
+        style={{ fontSize: "20px", cursor: "pointer", borderRadius: "2px" }}
+      >
+        Next
+      </button>
     </>
   );
 }
